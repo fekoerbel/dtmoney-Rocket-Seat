04210b21b62e1c70aec2446bed307602c9abc1f7
Felipe Koerbel dos Anjos <fekoerbel@hotmail.com>
2022-08-25 13:42:35 -0300

    modal styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root')
 
 
 export function App() {
+  const [isnewTransactionModalOpen, setIsnewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsnewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsnewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+      isOpen={isnewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //http://localhost:3000/api/transactions como este é o endereço, todas as chamadas /api serão como chamadas que queremos direcionar para miragejs  
+  
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );

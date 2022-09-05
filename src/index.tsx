befa3b7d814005a771aa08767545a7a906e60fa3
Felipe Koerbel dos Anjos <fekoerbel@hotmail.com>
2022-09-05 15:08:55 -0300

    modal changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api'; //http://localhost:3000/api/transactions como este é o endereço, todas as chamadas /api serão como chamadas que queremos direcionar para miragejs  
   
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+
+    })
+    this.post('/transaction', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
